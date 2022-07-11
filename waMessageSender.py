210b2b61e6f86455844bd5f93cd1e0d57c83ca4e
Renan <renanprodias2050@gmail.com>
2022-07-11 13:32:22 -0300

    Ajustes nas funções de envio de mensagens.

--- waMessageSender.py
+++ waMessageSender.py
@@ -14,6 +14,7 @@ desired_cap = {
 }
 
 #create a new broswer
+
 #browser = webdriver.Edge(executable_path='/Users/renand.pbarbono/Desktop/CEAD/WAmessageSender/msedgedriver',capabilities=desired_cap)
 browser  = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -36,6 +37,5 @@ for i, phone in enumerate(contacts['Mobile Phone']):
         time.sleep(30)
         browser.find_element_by_id_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)
         time.sleep(30)
-
 
          
